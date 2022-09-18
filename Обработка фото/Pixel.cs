bbaee909b702bd3779cb4dfa316489d64abde48f
Overlord6699 <ilya.velikolpnyyy@mail.ru>
2022-09-18 22:09:36 +0300

    first commit without optimisation

---
+++ Обработка фото/Pixel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Drawing;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Обработка_фото
+{
+    public class Pixel
+    {
+        public Point Point { get; set; }
+        public Color Color { get; set; }
+    }
+}
